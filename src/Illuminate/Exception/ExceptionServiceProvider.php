d0dcdc59d6e1b7cd1001094ec85a0f956ada496e
Taylor Otwell <taylorotwell@gmail.com>
2012-11-21 11:39:27 -0600

    moved service provider into component.

---
+++ src/Illuminate/Exception/ExceptionServiceProvider.php
@@ -0,0 +1,107 @@
+<?php namespace Illuminate\Exception;
+
+use Closure;
+use Illuminate\Support\ServiceProvider;
+use Symfony\Component\HttpKernel\Debug\ErrorHandler;
+use Symfony\Component\HttpKernel\Debug\ExceptionHandler as KernelHandler;
+
+class ExceptionServiceProvider extends ServiceProvider {
+
+	/**
+	 * Start the error handling facilities.
+	 *
+	 * @param  Illuminate\Foundation\Application  $app
+	 * @return void
+	 */
+	public function startHandling($app)
+	{
+		// By registering the error handler with a level of -1, we state that we want
+		// all PHP errors converted to ErrorExceptions and thrown, which provides
+		// a quite strict development environment, but prevents unseen errors.
+		$app['kernel.error']->register(-1);
+
+		$this->setExceptionHandler($app['exception.function']);
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @param  Illuminate\Foundation\Application  $app
+	 * @return void
+	 */
+	public function register($app)
+	{
+		$this->registerKernelHandlers($app);
+
+		$app['exception'] = $app->share(function()
+		{
+			return new Handler;
+		});
+
+		$this->registerExceptionHandler($app);
+	}
+
+	/**
+	 * Register the HttpKernel error and exception handlers.
+	 *
+	 * @param  Illuminate\Foundation\Application  $app
+	 * @return void
+	 */
+	protected function registerKernelHandlers($app)
+	{
+		$app['kernel.error'] = function()
+		{
+			return new ErrorHandler;
+		};
+
+		$app['kernel.exception'] = function() use ($app)
+		{
+			return new KernelHandler($app['config']['app.debug']);
+		};
+	}
+
+	/**
+	 * Register the PHP exception handler function.
+	 *
+	 * @param  Illuminate\Foundation\Application  $app
+	 * @return void
+	 */
+	protected function registerExceptionHandler($app)
+	{
+		$app['exception.function'] = function() use ($app)
+		{
+			return function($exception) use ($app)
+			{
+				$response = $app['exception']->handle($exception);
+
+				// If one of the custom error handlers returned a response, we will send that
+				// response back to the client after preparing it. This allows a specific
+				// type of exceptions to handled by a Closure giving great flexibility.
+				if ( ! is_null($response))
+				{
+					$response = $app->prepareResponse($response, $app['request']);
+
+					$response->send();
+				}
+				else
+				{
+					$app['kernel.exception']->handle($exception);
+				}
+			};
+		};
+	}
+
+	/**
+	 * Set the given Closure as the exception handler.
+	 *
+	 * This function is mainly needed for mocking purposes.
+	 *
+	 * @param  Closure  $handler
+	 * @return mixed
+	 */
+	protected function setExceptionHandler(Closure $handler)
+	{
+		return set_exception_handler($handler);
+	}
+
+}
